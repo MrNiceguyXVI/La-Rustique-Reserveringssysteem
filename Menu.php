647c08ccbacfffcf358bf4a65751587b93a4909c
Jason van Willigen <jasonvw@live.nl>
2020-11-09 18:32:02 +0100

    Menu under construction

--- Menu.php
+++ Menu.php
@@ -6,16 +6,39 @@
   <head>
     <title>La Rustique reserveringsysteem</title>
     <!--Bootstrap links (css and java)-->
-    <link rel="stylesheet" type="text/css" href="bootstap.css"/>
+    <link rel="stylesheet" type="text/css" href="bootstrap.css"/>
     <script src="Javascripts/bootstrap.js"></script>
   </head>
   <body>
+    <div class="container-fluid">
+      <!--Top bar-->
+      <div class="row" style="height:6vh">
+        <!--Logo-->
+        <div class="col-2 bg-primary ">
+          <div class="text-white">Logo</div>
+        </div>
 
-  <!--Here comes the div or container for the grid-->
+        <!--Page name-->
+        <div class="col-10 bg-info align-content-center">
+          
+          <div class="text-white" id="PageTitle">Welkom!</div>
+                   
+        </div>
+      </div>
 
-    <!--Here comes the container for the left container, set aside for the menu-->
+      <!--Menu and Selected page-->
+      <div class="row justify-content-center" style="height:94vh">
+        <!--Menu-->
+        <div class="col-2 bg-dark">
+          <div class="text-white">Menu</div>
+        </div>
 
-    <!--Here comes the Iframe container to show the webpages, as they're selected in the menu-->
-    <!--I'll add my classes to bootstrap's javascript document-->
+        <!--Selected page-->
+        <div class="col-10 bg-secondary">
+          <div class="text-white">Selected page</div>
+        </div>
+      </div>
+      <!--I'll add my classes to bootstrap's javascript document-->
+    </div>
   </body>
 </html>
