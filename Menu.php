283230a3bb6a0a58527641073d391e06aef6286c
Jason van Willigen <jasonvw@live.nl>
2020-11-16 09:02:16 +0100

    Added Jquery + popper

--- Menu.php
+++ Menu.php
@@ -6,23 +6,25 @@
   <head>
     <title>La Rustique reserveringsysteem</title>
     <!--Bootstrap links (css and java)-->
+    <script src="Javascripts/jquery-3.5.1.js"></script>
+    <script src="Javascripts/popper.js"></script>
+    <script src="Javascripts/bootstrap.bundle.js"></script>
     <link rel="stylesheet" type="text/css" href="bootstrap.css"/>
-    <script src="Javascripts/bootstrap.js"></script>
+    
   </head>
   <body>
     <div class="container-fluid">
       <!--Top bar-->
-      <div class="row" style="height:6vh">
+      <div class="row bg-white shadow-sm" style="height:6vh">
+
         <!--Logo-->
-        <div class="col-2 bg-primary ">
-          <div class="text-white">Logo</div>
+        <div class="col-2 p-0" style="display:flex;align-items: center; flex-wrap: wrap;">
+          <img src="Img/Placeholder.png" class="img-fluid" alt="Responsive image">
         </div>
 
         <!--Page name-->
-        <div class="col-10 bg-info align-content-center">
-          
-          <div class="text-white" id="PageTitle">Welkom!</div>
-                   
+        <div class="col-10" style="display:flex;align-items: center; flex-wrap: wrap;">          
+          <div class="text-black" id="PageTitle">Welkom!</div>
         </div>
       </div>
 
@@ -30,12 +32,25 @@
       <div class="row justify-content-center" style="height:94vh">
         <!--Menu-->
         <div class="col-2 bg-dark">
-          <div class="text-white">Menu</div>
+        <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active bg-dark border-0 text-white" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+    </div>
         </div>
 
         <!--Selected page-->
-        <div class="col-10 bg-secondary">
-          <div class="text-white">Selected page</div>
+        <div class="col-10 p-0">        
+          <!--<div class="embed-responsive">
+            <iframe class="embed-responsive-item" src=""></iframe>
+          </div>-->
+          <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
+      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
+      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
+      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+    </div>
         </div>
       </div>
       <!--I'll add my classes to bootstrap's javascript document-->
